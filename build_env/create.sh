baaffd3356827b72e8b93bcef817fa16bab93bdf
olliejohnson <oliver.joseph.johnson@gmail.com>
2021-12-04 22:10:26 -0500

    first commit

---
+++ build_env/create.sh
@@ -0,0 +1,10 @@
+echo "Dist Root: ${DIST_ROOT:?}"
+echo "LFS: ${LFS:?}"
+
+if ! test $(whoami) == "distbuild" ; then
+    echo "Must run as distbuild!"
+    exit -1
+fi
+
+echo "Creating build enviroment..."
+
